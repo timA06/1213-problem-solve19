469ffe93b0c53cf146681cd924719a76819957b2
Nadia Najjar <nanajjar@uncc.edu>
2025-04-13 22:08:14 -0400

    starter code Circle

---
+++ Circle.java
@@ -0,0 +1,51 @@
+
+/*
+ * ITSC 1213 Module 12
+ * The Circle class represents a circle with a radius and color.
+ */
+
+/**
+ * This class describes a circle with a given radius and color.
+ *
+ * @author nanajjar
+ * @version Spring 25
+ */
+public class Circle {  
+    // private instance variable, not accessible from outside this class
+    private double radius;
+    private Color color;
+    private double area;
+
+    // The default constructor with no argument.
+    // It sets the radius and color to their default value.
+    public Circle() {
+        radius = 1.0;
+        this.color = "red";
+    }
+
+    // 2nd constructor with given radius and color
+    public Circle(double r, String clr) {
+        radius = r;
+        color = clr;
+    }
+
+    // A public method for retrieving the radius
+    public double getRadius() {
+        return radius;
+    }
+
+    // A public method for retrieving the color of circle
+    public Color getColor() {
+        return color;
+    }
+
+    // A public method for retrieving the area of circle
+    public double getArea() {
+        calculateArea();
+        return area;
+    }
+    // A private method for computing the area of circle
+    private void calculateArea() {
+        area = radius * radius * Math.PI;
+    }
+}
