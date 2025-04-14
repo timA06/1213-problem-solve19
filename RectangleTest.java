5b9ebf65cd3fbff7d419398bd753c26cfecfba50
Nadia Najjar <nanajjar@uncc.edu>
2025-04-13 21:31:57 -0400

    starter code RectangleTest

---
+++ RectangleTest.java
@@ -0,0 +1,9 @@
+import java.awt.Rectangle;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(10, 10, 40, 30);
+        System.out.println("box1: " + box1);
+    }
+
+}
